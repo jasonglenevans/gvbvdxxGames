831061b5f6d3a1c941f3efd36b1ed292b4fc36f0
jasonglenevans <56980043+jasonglenevans@users.noreply.github.com>
2022-03-13 17:28:25 -0400

    Upload from shell script

--- upload.sh
+++ upload.sh
@@ -3,4 +3,4 @@ git add .
 git commit -m "Upload from shell script"
 git branch -M main
 git remote add origin https://github.com/jasonglenevans/gvbvdxxGames.git
-git push -u origin main
+git push -f origin main
